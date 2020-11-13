8f2c4fef7b6867666ecfd865e7d8ee53444f8634
DaveLClarkI <davelclark1@gmail.com>
2020-11-13 11:22:00 -0500

    Add files via upload

---
+++ IESLDSOC.C
@@ -0,0 +1,7 @@
+       01  IESLDSO-COMMAREA.                                                    
+         03  LDSO-NET-USERID           PIC  X(64).                              
+         03  LDSO-NET-PASSWORD         PIC  X(64).                              
+         03  LDSO-RET-CODE             PIC S9(09)   BINARY.                     
+         03  LDSO-LDAP-CODE            PIC S9(09)   BINARY.                     
+         03  LDSO-VSE-USERID           PIC  X(8).                               
+         03  LDSO-VSE-PASSWORD         PIC  X(8).                               
